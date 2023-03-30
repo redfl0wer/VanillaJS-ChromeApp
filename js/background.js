9b83f46edcbc698392b30d73e2a91dd6709dc306
WooChanLee1 <125115138+redfl0wer@users.noreply.github.com>
2023-03-30 14:49:34 +0000

    To-do / made a list of to-do

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 //image source : unsplash.com
 
-const images = ["img/andrea-davis-zGP_2B5G5Ok-unsplash.jpg", "img/boxed-water-is-better-tbhNNRIWxig-unsplash.jpg", "img/jonas-denil-4SuuEYOWYBg-unsplash.jpg", "img/marek-piwnicki-9TKW2zO5uTo-unsplash.jpg", "img/masahiro-miyagi-O1NYX40Du2g-unsplash.jpg", "img/mos-design-169lPCb09AA-unsplash.jpg", "img/mos-design-oF8nBV_sfJI-unsplash.jpg", "img/remy_loz-w9_mcsegoKY-unsplash.jpg", "img/wolfgang-hasselmann-Hmrx_cZWC3s-unsplash.jpg", "img/xavier-photography-wEL2u7wQhnY-unsplash.jpg"];
+const images = ["andrea-davis-zGP_2B5G5Ok-unsplash.jpg", "boxed-water-is-better-tbhNNRIWxig-unsplash.jpg", "jonas-denil-4SuuEYOWYBg-unsplash.jpg", "marek-piwnicki-9TKW2zO5uTo-unsplash.jpg", "masahiro-miyagi-O1NYX40Du2g-unsplash.jpg", "mos-design-169lPCb09AA-unsplash.jpg", "mos-design-oF8nBV_sfJI-unsplash.jpg", "remy_loz-w9_mcsegoKY-unsplash.jpg", "wolfgang-hasselmann-Hmrx_cZWC3s-unsplash.jpg", "xavier-photography-wEL2u7wQhnY-unsplash.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `${chosenImage}`;
+bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
