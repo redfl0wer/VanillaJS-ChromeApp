e6604db0e445ebb920c05054a046e1b4012ad727
WooChanLee1 <125115138+redfl0wer@users.noreply.github.com>
2023-04-02 10:54:39 +0000

    add weather information

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,17 @@
+const API_KEY = "9f3c7b0068517f9cc7031c948252b84d";
+
 function onGeoOk(position){
-  console.log(position);
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+    const weather = document.querySelector("#weather span:first-child") 
+    const city = document.querySelector("#weather span:last-child") 
+    city.innerText = data.name; 
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError(){
