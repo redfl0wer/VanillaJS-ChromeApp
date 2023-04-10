4cbda03ac2b4f8e72095326469cd63a02faf4c58
WooChanLee1 <125115138+redfl0wer@users.noreply.github.com>
2023-04-10 14:52:02 +0000

    delete background opacity, position:absolute to objects & repositioning

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("h2#clock");
+const date = new Date();
 
 function getClock(){
   const date = new Date();
--- js/greetings.js
+++ js/greetings.js
@@ -5,6 +5,8 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+let greeting_word;
+
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -12,13 +14,29 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
   loginForm.classList.remove("login-form");
-  clock.style.transform = 'translate(442px, -278px) scale(0.5)';
-  clock.style.transition = 'all 0.5s ease';
+  clock.style.transform = 'translate(18vw, -38.3vh) scale(0.5)';
+  clock.style.transition = 'all 1s ease';
   clock.style.fontSize = '110px';
 }
 
+if (String(date.getHours()) >= 6 && String(date.getHours()) <= 11){
+  greeting_word = "Good Morning";
+}
+else if (String(date.getHours()) >= 12 && String(date.getHours()) <= 17){
+  greeting_word = "Good Afternoon";
+}
+else if (String(date.getHours()) >= 18 && String(date.getHours()) <= 20){
+  greeting_word = "Good Evening";
+}
+else if (String(date.getHours()) >= 21 && String(date.getHours()) <= 23){
+  greeting_word = "Good Night";
+}
+else{
+  greeting_word = "It's Midnight";
+}
+
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}!`;
+  greeting.innerText = `${greeting_word}, ${username}.`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -30,6 +48,6 @@ if (savedUsername == null) {
 } else {
   loginForm.classList.remove("login-form");
   paintGreetings(savedUsername);
-  clock.style.transform = 'translate(442px, -278px) scale(0.5)';
+  clock.style.transform = 'translate(18vw, -38.3vh) scale(0.5)';
   clock.style.fontSize = '110px';
 }
