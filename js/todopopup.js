6ca2997f27962f97ebd1aa806b3bdb1c29d4f2c8
WooChanLee1 <125115138+redfl0wer@users.noreply.github.com>
2023-04-14 13:58:30 +0000

    popup effect2

--- js/todopopup.js
+++ js/todopopup.js
@@ -1,10 +1,16 @@
 const todoButton = document.querySelector(".todo-pop");
 const todoScreen = document.querySelector(".todo-container");
+const todoCloseButton = document.querySelector(".todo-close-button");
 
 todoButton.addEventListener("click", openTodo);
+todoCloseButton.addEventListener("click", closeTodo);
 
 function openTodo(event){
   event.preventDefault();
   todoScreen.classList.remove('hidden');
 }
 
+function closeTodo(event){
+  event.preventDefault();
+  todoScreen.classList.add('hidden');
+}
